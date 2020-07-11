8b4298e8a2eee4850a35927485ab3f68b9c1a6b8
Luciano dii Souza <lucianodiisouza@hotmail.com>
2020-07-10 22:14:55 -0300

    walkinng inside an object

---
+++ src/components/Pill.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span>{{ name }}</span>
+    <span>{{ amount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "amount"]
+};
+</script>
+
+<style>
+</style>
